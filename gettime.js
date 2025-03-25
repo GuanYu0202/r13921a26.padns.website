177fc02301e305a79805d3e38318fc28e62ba66d
GuanYu0202 <r13921a26@ntu.edu.tw>
2025-03-25 12:28:25 +0800

    Lab04 update!

--- gettime.js
+++ gettime.js
@@ -3,14 +3,14 @@ function showTime()
     let now = new Date();
     let options = {year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true, timeZone: 'Asia/Taipei'};
     let formattedTime = new Intl.DateTimeFormat('zh-TW', options).format(now);
-    document.querySelector('currentTime').innerHTML = formattedTime;
+    document.querySelector('.currentTime').innerHTML = formattedTime;
 }
 
 // for board.js
 function getFormattedTime() 
 {
     let now = new Date();
-    let options = {month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true, timeZone: 'Asia/Taipei'};
+    let options = {hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true, timeZone: 'Asia/Taipei'};
     return new Intl.DateTimeFormat('zh-TW', options).format(now);
 }
 
