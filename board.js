6237e733fced3400933b6c22b9853e4ccca51801
GuanYu0202 <r13921a26@ntu.edu.tw>
2025-03-25 10:34:33 +0800

    Lab04 update!

---
+++ board.js
@@ -0,0 +1,48 @@
+// Part 1: message board
+// boardcastchannel to implement the instant message board
+const board = new BroadcastChannel("chat_channel");
+
+// eavesdrop message event
+board.onmessage = (event) => 
+{displayMessage(event.data.name, event.data.message);};
+
+// func of adding message 
+function addMessage() 
+{
+	// enter name&message
+    const name = document.getElementById("name_input").value.trim();
+    const message = document.getElementById("message_input").value.trim();
+	
+	// check all data is ready on!
+    if (!name || !message) return alert("Please enter your name and message!");
+	
+	// post the message
+    board.postMessage({ name, message });
+	
+	// show the message
+    displayMessage(name, message);
+	
+	// clear the frame of message
+    document.getElementById("message_input").value = "";
+}
+
+// func of showing message
+function displayMessage(name, message) 
+{
+	// create <p> to place the message
+    const msg_emt = document.createElement("p");
+	
+	// post the message to <div> board
+    msg_emt.classList.add("message");
+    msg_emt.innerHTML = `<em>${name}:</em> ${message}`; // Italic 
+    document.getElementById("board").appendChild(msg_emt);
+}
+
+// Part 2: visitor counter
+let visitor_cnt = sessionStorage.getItem("visitorCount") || 0;
+visitor_cnt++;
+sessionStorage.setItem("visitorCount", visitor_cnt);
+
+// update number
+document.addEventListener("DOMContentLoaded", () => 
+{document.getElementById("visitorCount").innerText = visitor_cnt;});
--- gettime.js
+++ gettime.js
@@ -1,4 +1,5 @@
-function showTime() {
+function showTime() 
+{
     let now = new Date();
     let options = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true, timeZone: 'Asia/Taipei' };
     let formattedTime = new Intl.DateTimeFormat('zh-TW', options).format(now);
