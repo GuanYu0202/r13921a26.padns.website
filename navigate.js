608a64086d9caef466e2f9cd41c5dfab6dfd2d65
GuanYu0202 <r13921a26@ntu.edu.tw>
2025-03-29 21:53:48 +0800

    Lab05 update!
    1. main page, about page, user list page added.
    2. navigator added.

---
+++ navigate.js
@@ -0,0 +1,22 @@
+function loadPage(page) 
+{
+	// utilize Fetch API to load content dynamically
+	fetch(`/pages/${page}.html`)
+		.then(response => response.text())
+		.then(html => {
+			document.querySelector(".content").innerHTML = html;
+			// do not reload the whole page
+			window.history.pushState({}, "", "#" + page);
+		})
+		.catch(error => console.error("Loading Error:", error));
+}
+
+window.addEventListener("popstate", function() {
+	let page = location.hash.substring(1) || "main";
+	loadPage(page);
+});
+
+window.onload = function() {
+	let page = location.hash.substring(1) || "main";
+	loadPage(page);
+};
