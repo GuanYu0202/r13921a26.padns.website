74e6b9620b51e07305eb29e7a49be5df494bb409
GuanYu0202 <r13921a26@ntu.edu.tw>
2025-03-31 01:58:22 +0800

    Lab05 update
    attempt to connect frontend and backend

--- navigate.js
+++ navigate.js
@@ -6,25 +6,31 @@ function loadPage(page)
 		.then(html => {
 			document.getElementById("content").innerHTML = html;
 			// do not reload the whole page
-			window.history.pushState({}, "", "#" + page);
+			window.history.pushState({}, "", "locate?p=" + page);
 		})
 		.catch(error => console.error("Loading Error:", error));
 }
 
-window.addEventListener("popstate", function() {
+function openSignupWindow() 
+{
+	const signupUrl = "pages/register.html";
+	window.open(signupUrl, "Signup", "width=500,height=600,noopener");
+}
+
+function getCurrentPage() 
+{
+	const params = new URLSearchParams(window.location.search);
+	return params.get("p");
+}
+
+window.addEventListener("popstate", function() 
+{
 	let page = location.hash.substring(1);
     if (page) {loadPage(page);} 
-	else {
-        // the content of my initial page
-        document.getElementById("content").innerHTML = `
-            <h1 class="title"><b>Welcome to r13921a26's Website!!</b></h1>
-            <p>Click on the menu above to navigate through the different pages!</p>
-            <time class="currentTime"></time>
-        `;
-    }
 });
 
-document.addEventListener("DOMContentLoaded", function() {
-	let page = location.hash.substring(1);
-    if (page) {loadPage(page);}
+document.addEventListener("DOMContentLoaded", function() 
+{
+	let page = getCurrentPage();
+	if (page) { loadPage(page); }
 });
---
+++ register.js
@@ -0,0 +1,65 @@
+const supabase = window.supabase.createClient(
+	"https://egrpteffoyaajqxgaepx.supabase.co", 
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVncnB0ZWZmb3lhYWpxeGdhZXB4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNjExMzcsImV4cCI6MjA1ODczNzEzN30.uYSZ-yINbOVQMqHY4FXswSCPRMvSAFTbiwhWHFdQ6jc"
+)
+
+async function register() 
+{
+	const username = document.getElementById("username").value;
+	const email = document.getElementById("email").value;
+	const password = document.getElementById("password").value;
+	const confirmPassword = document.getElementById("confirmPassword").value;
+	const errorMsg = document.getElementById("signupError");
+
+	if (password !== confirmPassword) 
+	{
+		errorMsg.innerText = "Passwords do not match.";
+		return;
+	}
+
+	// email available
+	const { user, error } = await supabase.auth.signUp(
+	{
+		email: email,
+		password: password,
+	});
+
+	if (error) 
+	{
+		errorMsg.innerText = error.message;
+		return;
+	}
+	
+	// update data to database table
+	const { error: insertError } = await supabase
+		.from("users")
+		.insert([
+		{
+			id: user.id,
+			username: username,
+			email: email,
+			user_id: user.id
+		}
+		]);
+
+	if (insertError) 
+	{
+		errorMsg.innerText = insertError.message;
+		return;
+	}
+	
+	alert("Registration successful, please check the verification letter.");
+	if (user) 
+	{
+		await supabase.from("profiles").insert([{ id: user.id, username: username }]);
+	}
+
+	if (window.opener) 
+	{
+		window.close();
+	} 
+	else 
+	{
+		window.location.href = "index.html";
+	}
+}
